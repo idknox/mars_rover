9d22b413e3928d72d688ecd43fcf1ebaa26500df
Ian Knox <knoxid@gmail.com>
2014-11-09 16:47:36 -0700

    2nd rover moves

--- spec/mission_spec.rb
+++ spec/mission_spec.rb
@@ -4,14 +4,14 @@ require 'pp'
 describe Mission do
   before :each do
     @rover1 = Rover.new(1, 2, 'N')
-    @rover2 = Rover.new(3, 3, 'E')
+    @rover2 = Rover.new(2, 2, 'E')
     @mission = Mission.new([5, 5], @rover1, @rover2)
   end
 
   it 'instantiates with a grid' do
     expected_grid = [[0, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0],
-                     [0, 1, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [0, 1, 1, 0, 0],
                      [0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0]]
 
@@ -21,8 +21,8 @@ describe Mission do
   it 'can move rover N' do
     @mission.command_rover(@mission.rovers.first, 'M')
     expected_grid = [[0, 0, 0, 0, 0],
-                     [0, 1, 0, 1, 0],
-                     [0, 0, 0, 0, 0],
+                     [0, 1, 0, 0, 0],
+                     [0, 0, 1, 0, 0],
                      [0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0]]
 
@@ -33,7 +33,7 @@ describe Mission do
     @mission.command_rover(@mission.rovers.first, 'R')
     @mission.command_rover(@mission.rovers.first, 'M')
     expected_grid = [[0, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0],
+                     [0, 0, 0, 0, 0],
                      [0, 0, 1, 0, 0],
                      [0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0]]
@@ -47,8 +47,8 @@ describe Mission do
 
     @mission.command_rover(@mission.rovers.first, 'M')
     expected_grid = [[0, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0],
                      [0, 0, 0, 0, 0],
+                     [0, 0, 1, 0, 0],
                      [0, 1, 0, 0, 0],
                      [0, 0, 0, 0, 0]]
 
@@ -60,8 +60,8 @@ describe Mission do
 
     @mission.command_rover(@mission.rovers.first, 'M')
     expected_grid = [[0, 0, 0, 0, 0],
-                     [0, 0, 0, 1, 0],
-                     [1, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [1, 0, 1, 0, 0],
                      [0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0]]
 
@@ -71,11 +71,23 @@ describe Mission do
   it 'can give rover multiple commands' do
     @mission.instructions_to_rover(@mission.rovers.first, 'LMLMLMLMM')
     expected_grid = [[0, 0, 0, 0, 0],
-                     [0, 1, 0, 1, 0],
-                     [0, 0, 0, 0, 0],
+                     [0, 1, 0, 0, 0],
+                     [0, 0, 1, 0, 0],
                      [0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0]]
     expect(@mission.grid).to eq(expected_grid)
     expect(@rover1.direction).to eq('N')
+
+    @mission.instructions_to_rover(@mission.rovers.last, 'MMRMMRMRRM')
+    expected_grid = [[0, 0, 0, 0, 0],
+                     [0, 1, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 1]]
+
+    puts ''
+    pp @mission.grid
+    expect(@mission.grid).to eq(expected_grid)
+    expect(@rover2.direction).to eq('E')
   end
 end
