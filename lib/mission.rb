3d50ebe46f2c489746724c5fb011994463976e81
Ian Knox <knoxid@gmail.com>
2014-11-10 00:49:56 -0700

    refactored mission

--- lib/mission.rb
+++ lib/mission.rb
@@ -18,23 +18,17 @@ class Mission
   def instructions_to_rover(rover, instructions)
     old_position = rover.position
     instructions.chars.each {|command| command_rover(rover, command)}
-    @grid[@grid.length - old_position.last - 1][old_position.first] = 0
-    @grid[@grid.length - rover.position.last - 1][rover.x_coord] = 1
+    @grid[inverse_y_coordinate(old_position)][old_position.first] = 0
+    @grid[inverse_y_coordinate(rover.position)][rover.x_coord] = 1
   end
 
   private
 
   def set_starting_positions
-    @rovers.each do |rover|
-      @grid[inverse_y_coordinate(rover)][rover.x_coord] = 1
-    end
+    @rovers.each {|rover| @grid[inverse_y_coordinate(rover.position)][rover.x_coord] = 1 }
   end
 
-  def inverse_y_coordinate(rover)
-    @grid.length - rover.y_coord - 1
-  end
-
-  def rover_can_move?(rover, command)
-
+  def inverse_y_coordinate(coord)
+    @grid.length - coord.last - 1
   end
 end
